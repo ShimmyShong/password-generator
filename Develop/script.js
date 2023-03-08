10252c46b61e985bd660d8cb0144f86e25e000cc
unknown <lattabun@gmail.com>
2023-03-08 18:33:30 -0500

    added password content questions

--- Develop/script.js
+++ Develop/script.js
@@ -3,8 +3,42 @@ let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let specialChars = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", ">", "=", "?", "@", "[", "/", "]", "^", "_", "`", "{", "}", "|", "~"];
+let passArray = [];
 
-console.log(specialChars[1]);
+// debugging here
+let randomIndex = 0;
+
+var randomIndexFunction = function(){
+  randomIndex = Math.floor(Math.random() * passArray.length);
+  return randomIndex;
+}
+
+let passLength = prompt("How many characters would you like to have in your password?");
+
+let firstQuestion = prompt("Would you like lowercase characters in your password? \n y/n");
+let secondQuestion = prompt("Would you like uppercase characters in your password? \n y/n");
+let thirdQuestion = prompt("Would you like numbers in your password? \n y/n");
+let fourthQuestion = prompt("Would you like special characters in your password? \n y/n");
+
+if(firstQuestion == "y"){
+  passArray = passArray.concat(lowerCase);
+}
+if(secondQuestion == "y"){
+  passArray = passArray.concat(upperCase);
+}
+if(thirdQuestion == "y"){
+  passArray = passArray.concat(numbers);
+}
+if(fourthQuestion == "y"){
+  passArray = passArray.concat(specialChars);
+}
+
+for (let i = 0; i < passLength; i++){
+  var passwordText = `${passArray[randomIndexFunction()]}`;
+  console.log(passwordText);
+}
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
