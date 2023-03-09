a05a5bc6fccd020a421baafeadc161682839cf50
unknown <lattabun@gmail.com>
2023-03-08 22:45:22 -0500

    pre question answer checking

--- Develop/script.js
+++ Develop/script.js
@@ -3,36 +3,59 @@ let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let specialChars = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", ">", "=", "?", "@", "[", "/", "]", "^", "_", "`", "{", "}", "|", "~"];
-let passArray = [];
-let passwordText = [];
 
 var generatePassword = function(){
+  // initializing variables and arrays that must be reset every time this function is called
   let randomIndex = 0;
+  let passArray = [];
+  let passwordText = [];
 
+  // grabs a new random index number every time this function is called
   var randomIndexFunction = function(){
     randomIndex = Math.floor(Math.random() * passArray.length);
     return randomIndex;
   }
 
   let passLength = prompt("How many characters would you like to have in your password? \nYou must have more than 8, but less than 128 characters.");
+  // turns this value into an integer
+  passLength = parseInt(passLength);
 
+  // checks to make sure the value is a number, and within the correct range
+  if(!passLength){
+    return passwordText = "(Please enter a number)";
+  }
   if (passLength < 8){
-    window.alert("You must choose more than 8 characters.");
-    return;
+    return passwordText = "(You must choose more than 8 characters)";
   }
   if (passLength > 128){
-    window.alert("You must choose less than 128 characters.");
-    return;
+    return passwordText = "(You must choose less than 128 characters)";
   }
+
+
+  // let secondQuestion = prompt("Would you like uppercase characters in your password? \n y/n");
+  // let thirdQuestion = prompt("Would you like numbers in your password? \n y/n");
+  // let fourthQuestion = prompt("Would you like special characters in your password? \n y/n");
 
   let firstQuestion = prompt("Would you like lowercase characters in your password? \n y/n");
-  let secondQuestion = prompt("Would you like uppercase characters in your password? \n y/n");
-  let thirdQuestion = prompt("Would you like numbers in your password? \n y/n");
-  let fourthQuestion = prompt("Would you like special characters in your password? \n y/n");
 
-  if(firstQuestion == "y"){
-    passArray = passArray.concat(lowerCase);
+  for(i = 0; i != 0; i + 0){
+
+    if(firstQuestion == "y"){
+      passArray = passArray.concat(lowerCase);
+      console.log(firstQuestion);
+      i++;
+    }
+    else if(firstQuestion == "n"){
+      console.log(firstQuestion);
+      i++;
+      return;
+    }
+    else{
+      window.alert("Please enter a proper answer.")
+      console.log(firstQuestion);
+    }
   }
+
   if(secondQuestion == "y"){
     passArray = passArray.concat(upperCase);
   }
@@ -44,11 +67,12 @@ var generatePassword = function(){
   }
 
   for (let i = 0; i < passLength; i++){
-    passwordText = passwordText.concat(passArray[randomIndexFunction()]); //`${passArray[randomIndexFunction()]}`;
+    passwordText = passwordText.concat(passArray[randomIndexFunction()]);
   }
 
-  // console.log(passwordText.join(""));
+  // the .join prints the array on one line without any brackets or commas in between
   return passwordText.join("");
+
 }
 
 
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Password Generator Starter Code
+# Password Generator
+
+## Description
+This is a website that creates a random password that can range from 8 to 128 characters. 
