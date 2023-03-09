204d79fd52b55c3f294b6297b8e8cc5e98cd758c
unknown <lattabun@gmail.com>
2023-03-08 19:35:01 -0500

    mostly finished password generator

--- Develop/script.js
+++ Develop/script.js
@@ -4,38 +4,51 @@ let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let specialChars = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", ">", "=", "?", "@", "[", "/", "]", "^", "_", "`", "{", "}", "|", "~"];
 let passArray = [];
+let passwordText = [];
 
-// debugging here
-let randomIndex = 0;
+var generatePassword = function(){
+  let randomIndex = 0;
 
-var randomIndexFunction = function(){
-  randomIndex = Math.floor(Math.random() * passArray.length);
-  return randomIndex;
-}
+  var randomIndexFunction = function(){
+    randomIndex = Math.floor(Math.random() * passArray.length);
+    return randomIndex;
+  }
 
-let passLength = prompt("How many characters would you like to have in your password?");
+  let passLength = prompt("How many characters would you like to have in your password? \nYou must have more than 8, but less than 128 characters.");
 
-let firstQuestion = prompt("Would you like lowercase characters in your password? \n y/n");
-let secondQuestion = prompt("Would you like uppercase characters in your password? \n y/n");
-let thirdQuestion = prompt("Would you like numbers in your password? \n y/n");
-let fourthQuestion = prompt("Would you like special characters in your password? \n y/n");
+  if (passLength < 8){
+    window.alert("You must choose more than 8 characters.");
+    return;
+  }
+  if (passLength > 128){
+    window.alert("You must choose less than 128 characters.");
+    return;
+  }
 
-if(firstQuestion == "y"){
-  passArray = passArray.concat(lowerCase);
-}
-if(secondQuestion == "y"){
-  passArray = passArray.concat(upperCase);
-}
-if(thirdQuestion == "y"){
-  passArray = passArray.concat(numbers);
-}
-if(fourthQuestion == "y"){
-  passArray = passArray.concat(specialChars);
-}
+  let firstQuestion = prompt("Would you like lowercase characters in your password? \n y/n");
+  let secondQuestion = prompt("Would you like uppercase characters in your password? \n y/n");
+  let thirdQuestion = prompt("Would you like numbers in your password? \n y/n");
+  let fourthQuestion = prompt("Would you like special characters in your password? \n y/n");
 
-for (let i = 0; i < passLength; i++){
-  var passwordText = `${passArray[randomIndexFunction()]}`;
-  console.log(passwordText);
+  if(firstQuestion == "y"){
+    passArray = passArray.concat(lowerCase);
+  }
+  if(secondQuestion == "y"){
+    passArray = passArray.concat(upperCase);
+  }
+  if(thirdQuestion == "y"){
+    passArray = passArray.concat(numbers);
+  }
+  if(fourthQuestion == "y"){
+    passArray = passArray.concat(specialChars);
+  }
+
+  for (let i = 0; i < passLength; i++){
+    passwordText = passwordText.concat(passArray[randomIndexFunction()]); //`${passArray[randomIndexFunction()]}`;
+  }
+
+  // console.log(passwordText.join(""));
+  return passwordText.join("");
 }
 
 
